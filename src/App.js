9a5d6f15d4c8edf55d38133adc0cc22f1fade7fe
Verenasworld <verena.felizidas@gmail.com>
2021-01-10 20:39:11 +0100

    Folge 81 fertig -add Header component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HomePage from "./pages/homepage/homepage.component";
 import React from "react";
 import {Route,Switch} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.comppnent";
 
 
 
 function App() {
     return (
         <div>
+            <Header/>
             <Switch>
                 <Route  exact path='/' component={HomePage}/>
                 <Route  path='/shop' component={ShopPage}/>
