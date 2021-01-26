6509f980b26c7174e8d2cebbe18ed56a40745200
Verenasworld <verena.felizidas@gmail.com>
2021-01-26 12:28:28 +0100

    Folge 101-104 fertig - implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -23,19 +23,29 @@ class SignUp extends Component {
             return;
         }
         try {
-            const { user } = await auth.createUserWithEmailAndPassword(email,password)
+            const { user } = await auth.createUserWithEmailAndPassword(email,password);
             await createUserProfileDocument (user, {displayName});
-            this.setState()
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            });
         }catch (error){
             console.error(error);}
-
+    };
+    handleChange = event => {
+        const {name, value } = event.target;
+        this.setState({[name]: value});
     }
+
+
 
     render() {
         const {displayName, email, password, confirmPassword} = this.state;
         return (
             <div className='sign-up'>
-                <h2 className='title'> i do nor have a account</h2>
+                <h2 className='title'> I do not have a account</h2>
                 <span>Sign Up with your email & password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
                     <FormInput
