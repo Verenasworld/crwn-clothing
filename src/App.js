7f4aa4d7beae9cf71c27309e62f4d5f9cd212bb6
Verenasworld <verena.felizidas@gmail.com>
2021-01-26 10:40:59 +0100

    Folge 87-101 fertig - storing user Data set up

--- src/App.js
+++ src/App.js
@@ -21,13 +21,29 @@ class App extends React.Component {
     unsubscribeFromAuth= null
 
     componentDidMount(){
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-            createUserProfileDocument(user);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    });
+
+                    }
+                )
+            }else {
+                this.setState({currentUser: userAuth});
+            }
+
         });
     }
 
     componentWillUnmount() {
-        this.unsubscribeFromAuth();
+        this.unsubcribeFromAuth();
     }
 
     render() {
